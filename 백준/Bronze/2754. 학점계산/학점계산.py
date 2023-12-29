51f60bf34a7b135b69b7343fd662802db91fe145
CHAN LEE <153058476+mldlcl2022@users.noreply.github.com>
2023-12-30 01:19:25 +0900

    [Bronze V] Title: 학점계산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,17 @@
+# C언어 성적 입력
+s = input()
+
+# C언어 평점 출력
+if s == 'A+' : print(4.3)
+elif s == 'A0' : print(4.0)
+elif s == 'A-' : print(3.7)
+elif s == 'B+' : print(3.3)
+elif s == 'B0' : print(3.0)
+elif s == 'B-' : print(2.7)
+elif s == 'C+' : print(2.3)
+elif s == 'C0' : print(2.0)
+elif s == 'C-' : print(1.7)
+elif s == 'D+' : print(1.3)
+elif s == 'D0' : print(1.0)
+elif s == 'D-' : print(0.7)
+elif s == 'F' : print(0.0)
