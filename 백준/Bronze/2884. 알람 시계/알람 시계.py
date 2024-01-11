cb8aa43cab19ba5f0cad66b214a976c7a5ec44e5
CHAN LEE <153058476+mldlcl2022@users.noreply.github.com>
2024-01-11 16:06:48 +0900

    [Bronze III] Title: 알람 시계, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,8 +1,9 @@
-# enter hour, minute
 h, m = map(int, input().split())
 
-# result
-if m >= 45 : print(h, m-45)
+if m - 45 >= 0 :
+    print(h, m - 45)
 else :
-    if h == 0 : print(23, m+60-45)
-    else : print(h-1, m+60-45)
+    if h - 1 < 0 :
+        print(23, m - 45 + 60)
+    else :
+        print(h - 1, m - 45 + 60)
