083b76b8a2a6a2bbf15d2eb9478d8875be61c63e
CHAN LEE <153058476+mldlcl2022@users.noreply.github.com>
2024-01-17 13:03:54 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,12 @@
+n,m = map(int,input().split())
+num_list = list(map(int,input().split()))[:n]
+
+result = 0
+for i in range(n) :
+    for j in range(i+1, n) :
+        for k in range(j+1, n) :
+            if num_list[i] + num_list[j] + num_list[k] > m :
+                continue
+            else :
+                result = max(result, num_list[i] + num_list[j] + num_list[k])
+print(result)
