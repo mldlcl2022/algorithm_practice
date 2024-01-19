ceb4f2f88a6a24dc567318dd1c1b70415c625657
CHAN LEE <153058476+mldlcl2022@users.noreply.github.com>
2024-01-19 11:26:42 +0900

    [Bronze III] Title: 직각삼각형, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.py
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -1,5 +1,7 @@
 while True :
-  a, b, c = map(int, input().split())
-  if a == b == c == 0 : break
-  if (sorted([a, b, c])[0]**2 + sorted([a, b, c])[1]**2) == sorted([a, b, c])[2]**2 : print('right')
-  else : print('wrong')
+    l = sorted(list(map(int, input().split())))
+
+    if l.count(0) == 3 : break
+
+    if l[0]**2+l[1]**2 == l[2]**2 : print('right')
+    else : print('wrong')
