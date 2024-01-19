eb575b1cf2fdf8c1f752257b6f96695014f5a6d5
CHAN LEE <153058476+mldlcl2022@users.noreply.github.com>
2024-01-19 11:20:44 +0900

    [Bronze I] Title: 단어 공부, Time: 108 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,9 +1,13 @@
-s = [i.upper() for i in list(input())]
+# 단어 입력
+w = input()
 
-n = []
-for w in list(set(s)) :
-    n.append(s.count(w))
-if n.count(max(n)) > 1 :
-    print('?')
-else :
-    print(list(set(s))[n.index(max(n))])
+# 알파벳 고윳값 저장
+a_list = [i for i in list(set(w.upper()))]
+
+# 각 알파벳의 갯수 저장
+n_list = [w.upper().count(i) for i in list(set(w.upper()))]
+
+# 단어에서 가장 많이 사용된 알파벳 출력
+if n_list.count(max(n_list))==1 :
+    print(a_list[n_list.index(max(n_list))])
+else : print('?')
