76d64773637235b195eda387d14bf2ae79479c5b
CHAN LEE <153058476+mldlcl2022@users.noreply.github.com>
2024-01-19 11:05:26 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,9 +1,6 @@
-# 단어 s 입력
-s = input()
-
-# 알파벳 전체 반복
-for i in [chr(j) for j in range(97,123)] :
-    # 알파벳이 입력한 단어 s에 있다면 위치 출력
-    try : print(s.index(i), end = ' ')
-    # 알파벳이 입력한 단어 s에 없다면 -1 출력
-    except : print(-1, end = ' ')
+s = list(input())
+for i in range(97,123) :
+    try :
+        print(s.index(chr(i)), end= ' ')
+    except :
+        print(-1, end= ' ')
