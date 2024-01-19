cebd2843058e07c06ccb2d0502c96f4c181f36fb
CHAN LEE <153058476+mldlcl2022@users.noreply.github.com>
2024-01-19 10:52:15 +0900

    [Bronze II] Title: 문자열 반복, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,9 +1,7 @@
-# 테스트 케이스의 개수 t 입력
 t = int(input())
 
-# 테스트 케이스만큼 반복
 for _ in range(t) :
-    # 반복 횟수 r, 반복할 문자열 s 입력
     r, s = input().split()
-    # 반복한 새로운 문자열 출력
-    print(''.join([i*int(r) for i in s[:20]]))
+    for i in list(s) :
+        print(i*int(r), end= '')
+    print()
