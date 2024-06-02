2372679067ba45a70593ce3eac57ba8d414bf05d
CHAN LEE <153058476+mldlcl2022@users.noreply.github.com>
2024-06-02 17:01:05 +0900

    [Bronze III] Title: 오븐 시계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,6 +1,9 @@
-a,b = map(int,input().split())
+# enter time
+a, b = map(int, input().split())
 c = int(input())
-if b+c < 60 : print(a,b+c)
+
+# result
+if b+c < 60 : print(a, b+c)
 else :
-    if a+((b+c)//60) < 24 : print(a+((b+c)//60),(b+c)%60)
-    else : print(a+((b+c)//60)-24,(b+c)%60)
+    if a+((b+c)//60) < 24 : print(a+((b+c)//60), (b+c)%60)
+    else : print(a+((b+c)//60)-24, (b+c)%60)
