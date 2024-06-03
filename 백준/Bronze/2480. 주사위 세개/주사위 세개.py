eb7d3ac3b5cab27959f3d1bdad02778f73054715
CHAN LEE <153058476+mldlcl2022@users.noreply.github.com>
2024-06-03 22:24:50 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,8 +1,6 @@
-# enter value
-a, b, c = map(int, input().split())
+a,b,c = map(int, input().split())
 
-# result
-if a == b == c : print(10000+(a*1000))
-elif a == b or a == c : print(1000+(a*100))
-elif b == c : print(1000+(b*100))
-else : print(max(a, b, c)*100)
+if a!=b & b!= c & c!=a : print(max(a,b,c)*100)
+elif a==b==c : print(10000+(a*1000))
+elif a==b or a==c : print(1000+(a*100))
+elif b==c : print(1000+(b*100))
