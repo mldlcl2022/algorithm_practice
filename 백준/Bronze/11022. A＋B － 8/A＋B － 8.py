74502969dc5dd3386e7092c1f95ccbaffa4a2471
CHAN LEE <153058476+mldlcl2022@users.noreply.github.com>
2024-06-03 22:38:40 +0900

    [Bronze V] Title: A+B - 8, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -1,9 +1,5 @@
-# enter t
 t = int(input())
 
-# result
 for i in range(t) :
-    # enter a, b
-    a, b = map(int, input().split())
-    # print
-    print('Case #{:.0f}: {:.0f} + {:.0f} = {:.0f}'.format(i+1, a, b, a+b))
+    a,b = map(int, input().split())
+    print('Case #{}: {} + {} = {}'.format(i+1, a, b, a+b))
