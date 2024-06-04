84978f031c013ce9d3a920e344cefa640f7f42bc
CHAN LEE <153058476+mldlcl2022@users.noreply.github.com>
2024-06-04 10:36:37 +0900

    [Bronze II] Title: 나머지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,13 +1,4 @@
-# 입력받을 초기 리스트 생성
-num_list = []
-
-# 나머지를 구할 값 입력
+l = []
 for _ in range(10) :
-    num_list.append(int(input()))
-
-# 42로 나눈 나머지로 다시 저장
-for i, num in enumerate(num_list) :
-    num_list[i] = num%42
-
-# 입력받은 값을 42로 나눈 나머지들의 서로 다른 값 개수
-print(len(set(num_list)))
+    l.append(int(input())%42)
+print(len(l)-(10-len(set(l))))
