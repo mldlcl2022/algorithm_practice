1a9399cbde407d1e8f8f9e261c3da9503512bad8
CHAN LEE <153058476+mldlcl2022@users.noreply.github.com>
2024-06-07 10:31:12 +0900

    [Bronze I] Title: 평균, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,11 +1,4 @@
-# 과목의 개수 n
 n = int(input())
-
-# 세준이의 실제 성적
-score = [int(i) for i in input().split()][:n]
-
-# 세준이의 최고 점수(최댓값)
-m = max(score)
-
-# 점수 조작 결과
-print(sum([i/m*100 for i in score])/n)
+l = list(map(int,input().split()))[:n]
+m = max(l)
+print(sum([i/m*100 for i in l])/n)
