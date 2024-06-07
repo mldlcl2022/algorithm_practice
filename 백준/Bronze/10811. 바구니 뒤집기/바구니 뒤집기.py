ca64440a76f16fef971ce756c4ae79a093ddd29e
CHAN LEE <153058476+mldlcl2022@users.noreply.github.com>
2024-06-07 10:21:08 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,20 +1,9 @@
-# 총 바구니 수 n, 공을 바꿀 횟수 m 입력
-n, m = map(int, input().split())
+n,m = map(int, input().split())
+l = [i for i in range(1,n+1)]
 
-# 초기 바구니 설정
-b = [i+1 for i in range(n)]
-
-# 임시 변수
-temp = []
-
-# 공을 바꿀 횟수만큼 반복
 for _ in range(m) :
-    # 바꿀 첫 번째 바구니 i, 마지막 바구니 j
-    i, j = map(int, input().split())
-    # i ~ j 번째 숫자 임시 저장
-    temp = b[i-1:j]
-    # i ~ j 번째 숫자 역순 변환
-    b[i-1:j] = temp[::-1]
+    i,j = map(int, input().split())
+    tmp = l[i-1:j][::-1]
+    l[i-1:j] = tmp
 
-# 결과 출력
-for i in b : print(i, end = ' ')
+for i in l : print(i, end= ' ')
