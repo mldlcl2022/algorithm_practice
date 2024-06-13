8aee0e0ee6de558ac8b78fbf08d5048de84c7bc0
CHAN LEE <153058476+mldlcl2022@users.noreply.github.com>
2024-06-13 10:01:02 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,23 +1,13 @@
-# 학점 총합 계산을 위한 변수
-c_sum = 0
-# (학점 X 과목평점)의 합을 계산하기 위한 변수
-g_sum = 0
-# 20 과목 입력
+g_index = {'A+':4.5, 'A0':4.0, 'B+':3.5, 'B0':3.0, 'C+':2.5, 'C0':2.0, 'D+':1.5, 'D0':1.0, 'F':0.0}
+
+d_all = 0
+dxg = 0
 for _ in range(20) :
-    # 과목명, 학점, 성적 입력
-    s, c, g = input().split()
-    # 성적이 P가 아닌 경우 학점 총합 계산
-    if g != 'P' : c_sum += float(c)
-    # 괴목평점에 따라 계산
-    if g == 'A+' : g_sum += float(c)*4.5
-    elif g == 'A0' : g_sum += float(c)*4
-    elif g == 'B+' : g_sum += float(c)*3.5
-    elif g == 'B0' : g_sum += float(c)*3
-    elif g == 'C+' : g_sum += float(c)*2.5
-    elif g == 'C0' : g_sum += float(c)*2
-    elif g == 'D+' : g_sum += float(c)*1.5
-    elif g == 'D0' : g_sum += float(c)*1
-    elif g == 'F' : g_sum += float(c)*0
-    else : pass
-# 평점 결과 출력
-print(g_sum/c_sum)
+    s,d,g = input().split()
+    if g == 'P' : pass
+    else :
+        # 학점의 총합 계산
+        d_all += float(d)
+        # 학점 x 평점 합 계산
+        dxg += (float(d) * g_index[g])
+print(dxg/d_all)
