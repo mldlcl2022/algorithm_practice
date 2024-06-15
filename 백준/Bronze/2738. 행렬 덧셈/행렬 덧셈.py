e7d368289ac482247f456dd41834166ec6b0a022
CHAN LEE <153058476+mldlcl2022@users.noreply.github.com>
2024-06-15 15:17:04 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,19 +1,10 @@
-# 행렬의 크기 n, m 입력
-n, m = map(int, input().split())
-
-# 행렬 계산을 위한 빈 리스트 생성
-a, b = [], []
-
-# a 행렬 값 입력
-for _ in range(n) :
-    a.append(list(map(int, input().split())))
-
-# b 행렬 값 입력
-for _ in range(n) :
-    b.append(list(map(int, input().split())))
-    
-# 행렬 합 결과 출력
+n,m = map(int, input().split())
+m1 = [];m2 = []
+result = []
+for i in range(n*2) :
+    if i < n : m1.append(list(map(int, input().split()))[:m])
+    else : m2.append(list(map(int, input().split()))[:m])
 for i in range(n) :
     for j in range(m) :
-        print(a[i][j]+b[i][j], end= ' ')
+        print(m1[i][j]+m2[i][j], end= ' ')
     print()
