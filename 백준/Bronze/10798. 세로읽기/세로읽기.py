5810ae8a5075a7d46aa0ee469af12ac9dcffdf08
CHAN LEE <153058476+mldlcl2022@users.noreply.github.com>
2024-06-16 13:13:30 +0900

    [Bronze I] Title: 세로읽기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,14 +1,7 @@
-# 빈 행렬 생성
-l = []
-# 행렬 값 입력
+m = []
 for _ in range(5) :
-    l.append(list(input()))
-# 각각의 배열은 최대 15개 글자로 이루어져 있으므로 15번 반복
-for i in range(15) :
-    # 1차원 배열은 총 5개로 5번 반복
-    for j in range(5) :
-        # 순서대로 출력
-        try :
-            print(l[j][i], end= '')
-        except :
-            pass
+    m.append(list(input()))
+for i in range(max([len(m[i]) for i in range(len(m))])) :
+    for j in range(len(m)) :
+        try : print(m[j][i], end= '')
+        except : pass
