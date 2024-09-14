b2ad61c8e34ea17f6c07dcd3b38e56f50907f4de
CHAN LEE <mldlcl2022@gmail.com>
2024-09-14 09:42:13 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 52 ms, Memory: 34036 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,21 +1,11 @@
 from collections import deque
 
 n,k = map(int, input().split())
-queue = deque([i for i in range(1,n+1)])    
-
-idx = 0
+queue = deque(range(1,n+1))
 result = deque()
+print('<', end= '')
 while True :
-    idx += 1
-    pop_num = queue.popleft()
-    if idx % k == 0 :
-        result.append(pop_num)
+    if len(queue) == 1 : print(f'{queue.popleft()}>');break
     else :
-        queue.append(pop_num)
-    if len(queue) == 0 :
-        break
-for i,r in enumerate(result) :
-    if i == 0 : print('<', end= '')
-    print(r, end= '')
-    if (i+1) == len(result) : print('>', end= '')
-    else : print(', ', end= '')
+        queue.rotate(-(k-1))
+        print(f'{queue.popleft()}, ', end= '')
