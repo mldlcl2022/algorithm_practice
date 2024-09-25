c1982ea71d27238ea86e3e374c84f686bea89107
CHAN LEE <mldlcl2022@gmail.com>
2024-09-25 13:24:13 +0900

    [Silver IV] Title: 큐, Time: 64 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -1,22 +1,33 @@
-import sys
 from collections import deque
+import sys
 
 input = sys.stdin.readline
 n = int(input())
-queue = deque()
-for _ in range(n) :
+dq = deque()
+
+for _ in range(n):
     command = list(input().split())
-    if command[0] == 'push' : queue.append(int(command[1]))
-    elif command[0] == 'pop' :
-        if len(queue) != 0 : print(queue.popleft())
-        else : print(-1)
-    elif command[0] == 'size' : print(len(queue))
-    elif command[0] == 'empty' :
-        if len(queue) == 0 : print(1)
-        else : print(0)
-    elif command[0] == 'front' :
-        if len(queue) != 0 : print(queue[0])
-        else : print(-1)
-    elif command[0] == 'back' :
-        if len(queue) != 0 : print(queue[-1])
-        else : print(-1)
+    if command[0] == 'push':
+        dq.append(command[1])
+    elif command[0] == 'pop':
+        try:
+            print(dq.popleft())
+        except:
+            print(-1)
+    elif command[0] == 'size':
+        print(len(dq))
+    elif command[0] == 'empty':
+        if len(dq) > 0:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == 'front':
+        try:
+            print(dq[0])
+        except:
+            print(-1)
+    elif command[0] == 'back':
+        try:
+            print(dq[-1])
+        except:
+            print(-1)
