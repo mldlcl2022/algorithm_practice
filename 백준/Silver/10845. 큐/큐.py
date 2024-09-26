79ccc65c2106346a952a5b71a94b49bde845b7e1
CHAN LEE <mldlcl2022@gmail.com>
2024-09-27 01:09:16 +0900

    [Silver IV] Title: 큐, Time: 56 ms, Memory: 34204 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -1,33 +1,19 @@
+import sys;input = sys.stdin.readline
 from collections import deque
-import sys
-
-input = sys.stdin.readline
-n = int(input())
-dq = deque()
-
-for _ in range(n):
-    command = list(input().split())
-    if command[0] == 'push':
-        dq.append(command[1])
-    elif command[0] == 'pop':
-        try:
-            print(dq.popleft())
-        except:
-            print(-1)
-    elif command[0] == 'size':
-        print(len(dq))
-    elif command[0] == 'empty':
-        if len(dq) > 0:
-            print(0)
-        else:
-            print(1)
-    elif command[0] == 'front':
-        try:
-            print(dq[0])
-        except:
-            print(-1)
-    elif command[0] == 'back':
-        try:
-            print(dq[-1])
-        except:
-            print(-1)
+q = deque([])
+for _ in range(int(input())) :
+    operation = list(input().split())
+    if operation[0] == 'push' : q.append(int(operation[1]))
+    elif operation[0] == 'pop' :
+        if len(q) == 0 : print(-1)
+        else : print(q.popleft())
+    elif operation[0] == 'size' : print(len(q))
+    elif operation[0] == 'empty' :
+        if len(q) == 0 : print(1)
+        else : print(0)
+    elif operation[0] == 'front' :
+        if len(q) == 0 : print(-1)
+        else : print(q[0])
+    elif operation[0] == 'back' :
+        if len(q) == 0 : print(-1)
+        else : print(q[-1])
