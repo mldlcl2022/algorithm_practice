8bf1275d5c2c2ecccca629d653bf6a8c09aa409e
CHAN LEE <mldlcl2022@gmail.com>
2024-10-08 16:41:03 +0900

    [Silver IV] Title: 스택 2, Time: 808 ms, Memory: 71112 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -1,17 +1,16 @@
 import sys;input = sys.stdin.readline
 stack = []
 for _ in range(int(input())) :
-    operation = input().rstrip()
-    if operation.split()[0] == '1' :
-        stack.append(int(operation.split()[1]))
-    elif operation.split()[0] == '2' :
-        try : print(stack.pop())
-        except : print(-1)
-    elif operation.split()[0] == '3' :
-        print(len(stack))
-    elif operation.split()[0] == '4' :
+    o = input().rstrip()
+    
+    if o.split()[0] == '1' : stack.append(int(o.split()[1]))
+    elif o == '2' :
+        if len(stack) != 0 : print(stack.pop())
+        else : print(-1)
+    elif o == '3' : print(len(stack))
+    elif o == '4' :
         if len(stack) == 0 : print(1)
         else : print(0)
-    elif operation.split()[0] == '5' :
-        try : print(stack[-1])
-        except : print(-1)
+    elif o == '5' :
+        if len(stack) != 0 : print(stack[-1])
+        else : print(-1)
