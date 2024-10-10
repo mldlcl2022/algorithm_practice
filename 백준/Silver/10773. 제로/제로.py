04a8cfd3cbc2a81fc48d7f14c0bf445677224e68
CHAN LEE <mldlcl2022@gmail.com>
2024-10-10 22:59:05 +0900

    [Silver IV] Title: 제로, Time: 64 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,9 +1,7 @@
 import sys;input = sys.stdin.readline
 s = []
 for _ in range(int(input())) :
-    num = int(input())
-    if num != 0 : s.append(num)
-    else :
-        if len(s) != 0 : s.pop()
-        else : pass
+    n = int(input())
+    if n != 0 : s.append(n)
+    else : s.pop()
 print(sum(s))
