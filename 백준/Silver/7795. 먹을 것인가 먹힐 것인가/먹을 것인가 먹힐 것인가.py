f07f91988db180e75acd7b80061fe9e3ff4bcb80
CHAN LEE <mldlcl2022@gmail.com>
2024-10-11 10:59:27 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 132 ms, Memory: 34596 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,8 +1,8 @@
 import sys;input = sys.stdin.readline
 for _ in range(int(input())) :
     n,m = map(int, input().split())
-    a = sorted(list(map(int, input().split()))[:n])
-    b = sorted(list(map(int, input().split()))[:m])
+    a = sorted(list(map(int, input().split())))
+    b = sorted(list(map(int, input().split())))
     cnt = 0
     pair = 0
     for i in range(n) :
