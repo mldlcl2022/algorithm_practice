e8b86475183c82f878b366385f196196ccea7841
CHAN LEE <mldlcl2022@gmail.com>
2024-10-11 11:16:15 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,10 +1,13 @@
 import sys;input = sys.stdin.readline
 for _ in range(int(input())) :
-    ps = input().rstrip()
-    s = []
-    for p in ps :
-        if p == '(' : s.append(1)
-        elif p == ')' : s.append(-1)
-        if sum(s) < 0 : break
-    if sum(s) == 0 : print('YES')
+    s = input().rstrip()
+    stack = []
+    for p in s :
+        if p == '(' : stack.append(p)
+        else :
+            if len(stack) == 0 : stack.append(p)
+            else :
+                if stack[-1] == '(' : stack.pop()
+                else : stack.append(p)
+    if len(stack) == 0 : print('YES')
     else : print('NO')
