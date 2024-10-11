3d4d3a0ff0360272a17e6232b532293b76c7ca8e
CHAN LEE <mldlcl2022@gmail.com>
2024-10-11 10:05:34 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 128 ms, Memory: 34492 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,10 +1,14 @@
 import sys;input = sys.stdin.readline
-import bisect
 for _ in range(int(input())) :
     n,m = map(int, input().split())
     a = sorted(list(map(int, input().split()))[:n])
     b = sorted(list(map(int, input().split()))[:m])
     cnt = 0
-    for num in b :
-        cnt += len(a[bisect.bisect_right(a, num):])
-    print(cnt)
+    pair = 0
+    for i in range(n) :
+        while True :
+            if cnt == m or a[i] <= b[cnt] :
+                pair += cnt
+                break
+            else : cnt += 1
+    print(pair)
