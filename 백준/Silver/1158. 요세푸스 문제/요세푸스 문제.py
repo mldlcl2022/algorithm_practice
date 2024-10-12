7985f619427dccc1fd6d61ec774fb6c3c5dbd97c
CHAN LEE <mldlcl2022@gmail.com>
2024-10-12 15:48:47 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 52 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,9 +1,9 @@
-# import sys;input = sys.stdin.readline
+import sys;input = sys.stdin.readline
 from collections import deque
 n,k = map(int, input().split())
-q = deque(list(range(1,n+1)))
+queue = deque(list(range(1,n+1)))
 result = []
-while q :
-    q.rotate(-(k-1))
-    result.append(str(q.popleft()))
-print('<', ', '.join(result)[:], '>', sep='')
+while queue :
+    queue.rotate(-(k-1))
+    result.append(queue.popleft())
+print('<'+', '.join(map(str, result))+'>')
