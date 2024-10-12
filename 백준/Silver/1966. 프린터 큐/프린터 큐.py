19b64da21abb59fd75963fcceda256138148db82
CHAN LEE <mldlcl2022@gmail.com>
2024-10-12 15:31:24 +0900

    [Silver III] Title: 프린터 큐, Time: 68 ms, Memory: 34028 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -2,14 +2,16 @@ import sys;input = sys.stdin.readline
 from collections import deque
 for _ in range(int(input())) :
     n,m = map(int, input().split())
-    queue = deque([(importance, idx) for idx, importance in enumerate(list(map(int, input().split())))])
-    paper = queue[m][0]
-    order = 0
-    while True :
-        if queue[0][0] < max(queue)[0] :
-            queue.append(queue.popleft())
-        else :
-            importance, idx = queue.popleft()
-            if idx == m : order += 1;break
-            else : order += 1
-    print(order)
+    importance_list = list(map(int, input().split()))
+    queue = deque([(importance, order) for order, importance in enumerate(importance_list)])
+
+    cnt = 0
+    search_paper = importance_list[m]
+    while queue :
+        importance, order = queue.popleft()
+        if len(queue) == 0 : cnt += 1
+        elif importance < max(queue)[0] : queue.append((importance, order))
+        elif importance >= max(queue)[0] :
+            if importance == search_paper and order == m : cnt += 1;break
+            else : cnt += 1
+    print(cnt)
