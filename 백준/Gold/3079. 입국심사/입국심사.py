bca5622ace6f8d184a191dba728a7848355eeef4
CHAN LEE <mldlcl2022@gmail.com>
2024-10-14 01:11:45 +0900

    [Gold V] Title: 입국심사, Time: 564 ms, Memory: 38964 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.py
+++ 백준/Gold/3079. 입국심사/입국심사.py
@@ -1,19 +1,16 @@
 import sys;input = sys.stdin.readline
 n,m = map(int, input().split())
-n_list = []
+tables = []
 for _ in range(n) :
-    n_list.append(int(input()))
+    tables.append(int(input()))
 
-min_time = min(n_list);max_time = max(n_list)*m
-result = max(n_list)*m
+min_time = min(tables)
+max_time = result = max(tables)*m
 while min_time <= max_time :
-    total = 0
-    mid = (min_time+max_time) // 2
-    for i in range(len(n_list)) :
-        total += mid // n_list[i]
-    if total >= m :
-        max_time = mid - 1
-        result = min(result, mid)
+    mid = (min_time+max_time)//2
+    if sum([mid//table for table in tables]) >= m :
+        max_time = mid-1
+        result = min(mid, result)
     else :
-        min_time = mid + 1
+        min_time = mid+1
 print(result)
