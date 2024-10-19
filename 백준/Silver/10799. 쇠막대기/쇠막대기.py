4ed32fa96eae21d9b9ecc0a7b866eda1380a05fd
CHAN LEE <mldlcl2022@gmail.com>
2024-10-19 17:29:05 +0900

    [Silver II] Title: 쇠막대기, Time: 56 ms, Memory: 31552 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -1,14 +1,14 @@
 import sys;input = sys.stdin.readline
 s = input().rstrip()
 stack = []
-result = 0
+cnt = 0
 for i in range(len(s)) :
     if s[i] == '(' : stack.append('(')
     else :
         if s[i-1] == '(' :
             stack.pop()
-            result += len(stack)
+            cnt += len(stack)
         else :
             stack.pop()
-            result += 1
-print(result)
+            cnt += 1
+print(cnt)
