f6d0aad810d6dcf682700300fe809c81ada44f37
CHAN LEE <mldlcl2022@gmail.com>
2024-10-22 13:57:26 +0900

    [Silver V] Title: 카드1, Time: 64 ms, Memory: 34148 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.py
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -1,10 +1,11 @@
 import sys;input = sys.stdin.readline
 from collections import deque
 n = int(input())
-cards = deque([i for i in range(1,n+1)])
-result = []
+cards = deque(list(range(1,n+1)))
+
 while cards :
-    result.append(cards.popleft())
-    if len(cards) == 0 : break
-    else : cards.append(cards.popleft())
-print(' '.join(map(str, result)))
+    pop_card = cards.popleft()
+    if len(cards) == 0 : print(pop_card)
+    else :
+        print(pop_card, end= ' ')
+        cards.append(cards.popleft())
