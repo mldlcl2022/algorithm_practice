002549ccaac33c092058320506d5c8f30f1e174e
CHAN LEE <mldlcl2022@gmail.com>
2024-11-03 16:55:22 +0900

    [Bronze III] Title: 최소, 최대, Time: 368 ms, Memory: 153832 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,8 +1,3 @@
-# enter n
 n = int(input())
-
-# number list
-num_list = list(map(int, input().split()))[:n]
-
-# result
-print(min(num_list), max(num_list))
+l = list(map(int, input().split()))[:n]
+print(min(l), max(l))
