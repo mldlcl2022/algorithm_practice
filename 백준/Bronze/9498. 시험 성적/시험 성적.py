b0726f340473a5d66dac9e6522115ae69c73c04d
CHAN LEE <mldlcl2022@gmail.com>
2024-11-03 16:42:19 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,8 +1,5 @@
-# enter score
 score = int(input())
-
-# result
-if   score >= 90 : print('A')
+if score >= 90 : print('A')
 elif score >= 80 : print('B')
 elif score >= 70 : print('C')
 elif score >= 60 : print('D')
