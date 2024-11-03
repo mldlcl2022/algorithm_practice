2c9e17cdd494965ea66cb4f6e57670145cd32477
CHAN LEE <mldlcl2022@gmail.com>
2024-11-03 16:33:52 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
@@ -1,7 +1,4 @@
-# enter a, b
-a, b = map(int, input().split())
-
-# result
-if   a > b  : print('>')
-elif a < b  : print('<')
-elif a == b : print('==')
+a,b = map(int, input().split())
+if a > b : print('>')
+elif a < b : print('<')
+else : print('==')
