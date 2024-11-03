846925c0cdfe5b20248657bcd676d1b828435858
CHAN LEE <mldlcl2022@gmail.com>
2024-11-03 17:07:34 +0900

    [Bronze II] Title: OX퀴즈, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -1,12 +1,16 @@
 t = int(input())
 for _ in range(t) :
-  ox = input()
-  n = 1
-  result = 0
-  for s in ox :
-    if s == 'O' :
-      result += n
-      n += 1
-    elif s == 'X' :
-      n = 1
-  print(result)
+    ox = input()
+    tmp = 0
+    score = 0
+    for i in range(len(ox)) :
+        if i == 0 :
+            if ox[i] == 'O' :
+                tmp += 1
+                score += tmp
+        else :
+            if ox[i] == 'O' :
+                tmp += 1
+                score += tmp
+            else : tmp = 0
+    print(score)
