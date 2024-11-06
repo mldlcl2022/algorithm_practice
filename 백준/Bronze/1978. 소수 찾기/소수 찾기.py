fc081754848b3f648946658a947099f96f99469b
CHAN LEE <mldlcl2022@gmail.com>
2024-11-06 10:10:06 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,10 +1,13 @@
 n = int(input())
 nums = list(set(map(int, input().split())))
-result = 0
+
+cnt = 0
 for num in nums :
-    cnt = 0
+    tmp = 0
     if num > 1 :
         for i in range(2,num) :
-            if num % i == 0 : cnt += 1
-        if cnt == 0 : result += 1
-print(result)
+            if num % i == 0 :
+                tmp += 1
+                break
+        if tmp == 0 : cnt += 1
+print(cnt)
