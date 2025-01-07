560ec9230a91a5d534e4c7717bd57b1d2b1d894b
CHAN LEE <mldlcl2022@gmail.com>
2025-01-07 10:29:39 +0900

    [Bronze V] Title: 나머지, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.py
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -1,8 +1,5 @@
-# enter a, b, c
-A, B, C = map(int, input().split())
-
-# results
-print((A+B)%C)
-print(((A%C)+(B%C))%C)
-print((A*B)%C)
-print(((A%C)*(B%C))%C)
+a,b,c = map(int, input().split())
+print((a+b)%c)
+print(((a%c)+(b%c))%c)
+print((a*b)%c)
+print(((a%c)*(b%c))%c)
