d108056d13d3cd47e4183f64291d76cbe0c85375
CHAN LEE <mldlcl2022@gmail.com>
2025-01-07 11:18:46 +0900

    [Bronze III] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,6 +1,6 @@
 num1 = int(input())
 num2 = int(input())
-
-for i in range(-1,-4,-1) :
-    print(num1*int(str(num2)[i]))
+print(num1*(num2%10))
+print(num1*(num2%100//10))
+print(num1*(num2//100))
 print(num1*num2)
