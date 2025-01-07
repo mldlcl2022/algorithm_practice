838330d148b36784097f0c902023fe066cb7a1db
CHAN LEE <mldlcl2022@gmail.com>
2025-01-07 13:38:32 +0900

    [Bronze V] Title: 사분면 고르기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -1,11 +1,6 @@
-# enter num
 x = int(input())
 y = int(input())
-
-# result
-if x > 0 :
-    if y > 0 : print(1)
-    else : print(4)
-else :
-    if y > 0 : print(2)
-    else : print(3)
+if x > 0 and y > 0 : print(1)
+elif x < 0 and y > 0 : print(2)
+elif x < 0 and y < 0 : print(3)
+elif x > 0 and y < 0 : print(4)
