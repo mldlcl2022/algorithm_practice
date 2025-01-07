1840c66378a1d292438caad721c870bfd5e11610
CHAN LEE <mldlcl2022@gmail.com>
2025-01-07 14:03:03 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,6 +1,4 @@
 a,b,c = map(int, input().split())
-
-if a!=b & b!= c & c!=a : print(max(a,b,c)*100)
-elif a==b==c : print(10000+(a*1000))
-elif a==b or a==c : print(1000+(a*100))
-elif b==c : print(1000+(b*100))
+if (a==b) and (b==c) and (a==c): print(10000+(a*1000))
+elif (a!=b) and (b!=c) and (a!=c) : print(max(a,b,c)*100)
+elif a==b or b==c or a==c : print(1000+(sorted([a,b,c])[1])*100)
