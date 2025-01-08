e20cb4a65a903f36c86d7033ce84939210526964
CHAN LEE <mldlcl2022@gmail.com>
2025-01-08 10:52:14 +0900

    [Bronze II] Title: 공 바꾸기, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,23 +1,9 @@
-# 총 바구니 수 n, 공을 바꿀 횟수 m 입력
-n, m = map(int, input().split())
-
-# 초기 바구니 설정
+n,m = map(int, input().split())
 b = [i+1 for i in range(n)]
-
-# 임시 변수
-temp = 0
-
-# 공을 바꿀 횟수만큼 반복
 for _ in range(m) :
-    # 바꿀 바구니 번호 1, 바꿀 바구니 번호 2
-    i, j = map(int, input().split())
-    # 임시 변수에 첫 번째 바구니 값 임시 저장
-    temp = b[i-1]
-    # 첫 번째 바구니 값을 두 번째 바구니 값으로 변경
+    i,j = map(int, input().split())
+    tmp = b[i-1]
     b[i-1] = b[j-1]
-    # 두 번째 바구니 값을 임시 저장한 첫 번째 바구니 값으로 변경
-    b[j-1] = temp
-
-# 결과 확인
-for num in b :
-    print(num, end = ' ')
+    b[j-1] = tmp
+for c in b :
+    print(c, end= ' ')
